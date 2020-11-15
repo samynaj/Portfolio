6011a6cab46a858e8b5b6fa287397931589f5c14
samynaj <samuelnnj@gmail.com>
2020-11-15 23:04:09 +0100

    changed App contents and added styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Navbar from './components/nav';
+import ParticlesBg from 'particles-bg';
+import Bounce from 'react-reveal/Bounce';
+import Slide from 'react-reveal/Slide';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import MailIcon from '@material-ui/icons/Mail';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Home from './components/home'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav'>
+        <Bounce duration={3000}>
+          <Navbar/>
+        </Bounce>
+      </div>
+      <Home/>
+      <Slide duration={1000} bottom>
+        <div className='links'>
+            <span className='btn-links'>
+                <MailIcon/> 
+            </span>
+            <span className='btn-links'>
+                <TwitterIcon/> 
+            </span>
+            <span className='btn-links'>
+                <LinkedInIcon/>
+            </span>
+            <span className='btn-links'>
+                <GitHubIcon/>
+            </span>
+        </div>
+      </Slide>
+      <ParticlesBg type='thick' color="#000" num={25} bg='true'/>
     </div>
   );
 }
