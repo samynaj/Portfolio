91a972118db84833872a38a37e222288519d0925
samynaj <samuelnnj@gmail.com>
2020-11-18 15:32:26 +0100

    edited project page

---
+++ src/pages/projects/projects.jsx
@@ -0,0 +1,13 @@
+import ParticlesBg from 'particles-bg';
+
+
+const Projects = () => {
+    return (
+        <div>
+            <h1>PROJECTS WORKED ON</h1>
+            <ParticlesBg type='lines' color="#000" num={500} bg='true'/>
+        </div>
+    )
+}
+
+export default Projects;
