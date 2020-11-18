a52fdc354cb2ceaef0cdf0193b47a2398698e205
samynaj <samuelnnj@gmail.com>
2020-11-18 15:30:41 +0100

    edited contact page

---
+++ src/pages/contacts/contacts.jsx
@@ -0,0 +1,13 @@
+import ParticlesBg from 'particles-bg';
+
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>WANT TO HIRE ME</h1>
+            <ParticlesBg type='lines' color="#000" num={500} bg='true'/>
+        </div>
+    )
+}
+
+export default Contact;
