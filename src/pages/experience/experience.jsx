926bab8579e194b4142b9c0e3e1d77c0e8747667
samynaj <samuelnnj@gmail.com>
2020-11-18 15:31:26 +0100

    edited experience page

---
+++ src/pages/experience/experience.jsx
@@ -0,0 +1,13 @@
+import ParticlesBg from 'particles-bg';
+
+
+const Experience = () => {
+    return (
+        <div>
+            <h1>PROFESSIONAL EXPERIENCES</h1>
+            <ParticlesBg type='lines' color="#000" num={500} bg='true'/>
+        </div>
+    )
+}
+
+export default Experience;
