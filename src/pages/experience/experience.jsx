d34e7111b880ebee25029f2ef86ce7a9809ae517
samynaj <samuelnnj@gmail.com>
2020-11-19 19:05:27 +0100

    minor fix

--- src/pages/experience/experience.jsx
+++ src/pages/experience/experience.jsx
@@ -9,7 +9,7 @@ const Experience = () => {
                 <h1>PROFESSIONAL EXPERIENCES</h1>
             </Slide>
             <Slide bottom duration={3000}>
-                <h3>STACK</h3>
+                <h3>STACKS</h3>
                 <p>I have good working experience using JavaScript, Typescript, and a bit of the PHP programming language.</p>
                 <p>Also I worked with React, Node.js, Express, Angular, jQuery, Bootstrap, Tailwind, Material UI, Next js frameworks and libraries.</p>
             </Slide>
