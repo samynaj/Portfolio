78285d1eb017b718cf47ce6f935d5fec4e61458c
samynaj <samuelnnj@gmail.com>
2020-11-19 18:43:26 +0100

    added technology stack

--- src/pages/experience/experience.jsx
+++ src/pages/experience/experience.jsx
@@ -1,10 +1,19 @@
 import ParticlesBg from 'particles-bg';
+import Slide from 'react-reveal/Slide'
 
 
 const Experience = () => {
     return (
         <div>
-            <h1>PROFESSIONAL EXPERIENCES</h1>
+            <Slide top duration={3000}>
+                <h1>PROFESSIONAL EXPERIENCES</h1>
+            </Slide>
+            <Slide bottom duration={3000}>
+                <h3>STACK</h3>
+                <p>I have good working experience using JavaScript, Typescript, and a bit of the PHP programming language.</p>
+                <p>Also I worked with React, Node.js, Express, Angular, jQuery, Bootstrap, Tailwind, Material UI, Next js frameworks and libraries.</p>
+            </Slide>
+            
             <ParticlesBg type='square' color="#000" num={10} bg='true'/>
         </div>
     )
