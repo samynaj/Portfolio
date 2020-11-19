692b9de0a5bb96b801c4d197a2fbc8a575cac805
samynaj <samuelnnj@gmail.com>
2020-11-19 12:58:39 +0100

    changed background animation

--- src/pages/experience/experience.jsx
+++ src/pages/experience/experience.jsx
@@ -5,7 +5,7 @@ const Experience = () => {
     return (
         <div>
             <h1>PROFESSIONAL EXPERIENCES</h1>
-            <ParticlesBg type='polygon' color="#000" num={5} bg='true'/>
+            <ParticlesBg type='square' color="#000" num={10} bg='true'/>
         </div>
     )
 }
