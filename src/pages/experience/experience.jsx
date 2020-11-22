224f39b70b3df2bdbc2b48a5157699b3b760012f
samynaj <samuelnnj@gmail.com>
2020-11-22 18:56:54 +0100

    import style sheet

--- src/pages/experience/experience.jsx
+++ src/pages/experience/experience.jsx
@@ -1,18 +1,19 @@
 import ParticlesBg from 'particles-bg';
-import Slide from 'react-reveal/Slide'
+import Slide from 'react-reveal/Slide';
+import './experience.scss'
 
 
 const Experience = () => {
     return (
-        <div>
+        <div className='root'>
             <Slide top duration={3000}>
                 <h1>PROFESSIONAL EXPERIENCES</h1>
             </Slide>
             <Slide bottom duration={3000}>
                 <h3>STACKS</h3>
-                <p>I have good working experience using JavaScript, Typescript, and a bit of the PHP programming language.</p>
-                <p>Also I worked with React, Node.js, Express, Angular, jQuery, Bootstrap, Tailwind, Material UI, Next js frameworks and libraries.</p>
             </Slide>
+            <p>I have good working experience using JavaScript, Typescript, and a bit of the PHP programming language.</p>
+            <p>Also I worked with React, Node.js, Express, Angular, jQuery, Bootstrap, Tailwind, Material UI, Next js frameworks and libraries.</p>
             
             <ParticlesBg type='square' color="#000" num={10} bg='true'/>
         </div>
