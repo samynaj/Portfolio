f189e8da84f4b2f2b15fa15fe6a1b9575ce975df
samynaj <samuelnnj@gmail.com>
2020-11-27 21:26:28 +0100

    edit projects page

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -3,7 +3,7 @@ import ParticlesBg from 'particles-bg';
 
 const Projects = () => {
     return (
-        <div>
+        <div className='root'>
             <h1>PROJECTS WORKED ON</h1>
             <ParticlesBg type='square' color="#000" num={10} bg='true'/>
         </div>
