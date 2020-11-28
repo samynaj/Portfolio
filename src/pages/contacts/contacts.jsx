61fcea2b62eb67984f26f22dd253354341e74e02
samynaj <samuelnnj@gmail.com>
2020-11-28 23:12:54 +0100

    added and arranged contact input elements

--- src/pages/contacts/contacts.jsx
+++ src/pages/contacts/contacts.jsx
@@ -1,30 +1,37 @@
 import ParticlesBg from 'particles-bg';
-import Slide from 'react-reveal/Slide';
+// import Slide from 'react-reveal/Slide';
 import TwitterIcon from '@material-ui/icons/Twitter';
-import MailIcon from '@material-ui/icons/Mail';
+// import MailIcon from '@material-ui/icons/Mail';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import './contact.scss';
 
 const Contact = () => {
     return (
         <div className='root'>
             <h1>WANT TO HIRE ME</h1>
-                <Slide duration={1000} bottom>
-                    <div className='links'>
-                        <span className='btn-links'>
-                            <MailIcon/> 
-                        </span>
-                        <span className='btn-links'>
-                            <TwitterIcon/> 
-                        </span>
-                        <span className='btn-links'>
-                            <LinkedInIcon/>
-                        </span>
-                        <span className='btn-links'>
-                            <GitHubIcon/>
-                        </span>
-                    </div>
-                </Slide>
+            <div className='contact'>
+                <form action="#" method='POST' className='contact-form'>
+                    <input type="text" placeholder='Full Name' className='contact-form-input'/>
+                    <input type="email" placeholder='Email Address' className='contact-form-input'/>
+                    <input type="text" placeholder='Subject' className='contact-form-input'/>
+                    <textarea className='contact-form-text' placeholder='Enter message here'></textarea>
+                    <button type='submit' name='button' className='contact-form-button'>Send Message</button>
+                </form>
+            </div>
+            
+            <div className='links'>
+                <span>
+                    <TwitterIcon size='md' className='btn-links'/> 
+                </span>
+                <span>
+                    <LinkedInIcon className='btn-links'/>
+                </span>
+                <span>
+                    <GitHubIcon className='btn-links'/>
+                </span>
+            </div>
+            
             <ParticlesBg type='square' color="#000" num={10} bg='true'/>
         </div>
     )
