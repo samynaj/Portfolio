de2e80e82efb05f0b9f34c00574c5fb1a394ffb4
samynaj <samuelnnj@gmail.com>
2020-11-30 12:08:42 +0100

    handled contact form

--- src/pages/contacts/contacts.jsx
+++ src/pages/contacts/contacts.jsx
@@ -5,17 +5,35 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import './contact.scss';
+import emailjs from 'emailjs-com';
+// const { REACT_APP_MY_SERVICE_ID, REACT_APP_MY_TEMPLATE_ID, REACT_APP_MY_USER_ID } = process.env.;
 
 const Contact = () => {
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm(
+            process.env.REACT_APP_MY_SERVICE_ID, 
+            process.env.REACT_APP_MY_TEMPLATE_ID, 
+            e.target, 
+            process.env.REACT_APP_MY_USER_ID)
+            .then((result) => {
+                e.target.reset();
+                alert("Message Sent, Thanks will get back to you shortly", result.text);
+            
+            }, (error) => {
+            alert("An error occurred, Please try again", error.text);
+          });
+      }
     return (
         <div className='root'>
             <h1>WANT TO HIRE ME</h1>
             <div className='contact'>
-                <form action="#" method='POST' className='contact-form'>
-                    <input type="text" placeholder='Full Name' className='contact-form-input'/>
-                    <input type="email" placeholder='Email Address' className='contact-form-input'/>
-                    <input type="text" placeholder='Subject' className='contact-form-input'/>
-                    <textarea className='contact-form-text' placeholder='Enter message here'></textarea>
+                <form className='contact-form' onSubmit={sendEmail}>
+                    <input type="text" name="user_name" placeholder='Full Name' className='contact-form-input'/>
+                    <input type="email" name="user_email" placeholder='Email Address' className='contact-form-input'/>
+                    <input type="text" name="subject" placeholder='Subject' className='contact-form-input'/>
+                    <textarea name="message" className='contact-form-text' placeholder='Enter message here'></textarea>
                     <button type='submit' name='button' className='contact-form-button'>Send Message</button>
                 </form>
             </div>
