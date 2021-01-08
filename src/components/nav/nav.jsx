41b0bb558f8869b08633e11edc9c29dd1bced0fc
samynaj <samuelnnj@gmail.com>
2021-01-08 23:47:33 +0100

    worked on navbar

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,17 +1,79 @@
-import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+// import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import './nav.scss';
-import HomeIcon from '@material-ui/icons/Home';
-import PeopleIcon from '@material-ui/icons/People';
-import ExplicitIcon from '@material-ui/icons/Explicit';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import CallIcon from '@material-ui/icons/Call';
-import { Link } from 'react-router-dom';
-import Tooltip from '@material-ui/core/Tooltip';
+// import HomeIcon from '@material-ui/icons/Home';
+// import PeopleIcon from '@material-ui/icons/People';
+// import ExplicitIcon from '@material-ui/icons/Explicit';
+// import GitHubIcon from '@material-ui/icons/GitHub';
+// import CallIcon from '@material-ui/icons/Call';
+// import { Link } from 'react-router-dom';
+// import Tooltip from '@material-ui/core/Tooltip';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Slide from '@material-ui/core/Slide';
+import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link"
 
-const Navbar = () => {
+const useStyles = makeStyles(theme => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  appbar: {
+    backgroundColor: "#111822"
+  },
+  
+}))
+
+const HideOnScroll = (props) => { 
+  const {children, window} = props;
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  )
+}
+
+const Navbar = (props) => {
+  const classes = useStyles();
   return (
     <div className='sidebar'>
-      <ProSidebar width='80px'>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar className={classes.appbar}>
+          <ToolBar className="toolbar">
+            <IconButton className="img-button">
+              <img src="https://i.pinimg.com/originals/93/d3/e3/93d3e31639a4d07613de9dccdc8bd5e8.png" alt="webdev"/>
+            </IconButton>
+            <div className="nav-buttons">
+              <Link href="#home" color="inherit">
+                <Button color="inherit">Home</Button>
+              </Link>
+              <Link href="#about" color="inherit">
+                <Button color="inherit">About</Button>
+              </Link>
+              <Link href="#projects" color="inherit">
+                <Button color="inherit">Projects</Button>
+              </Link>
+              <Link href="#testimony" color="inherit">
+                <Button color="inherit">Testimonials</Button>
+              </Link>
+              <Link href="#contact" color="inherit">
+                <Button color="inherit">Contact</Button>
+              </Link>
+              
+            </div>
+            
+          </ToolBar>
+        </AppBar>
+      </HideOnScroll>
+
+      {/* <ProSidebar width='80px'>
         <Menu className='menu'>
           <Tooltip title='Home' placement='right'>
             <MenuItem icon={<HomeIcon/>}>
@@ -39,7 +101,7 @@ const Navbar = () => {
             </MenuItem>
           </Tooltip>
         </Menu>
-      </ProSidebar>
+      </ProSidebar> */}
     </div>
     
   )
