b3366cae45e29f51df4b225bce80d0e29e9c6c73
samynaj <samuelnnj@gmail.com>
2021-01-08 23:48:01 +0100

    worked on about page

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,26 +1,31 @@
-import ParticlesBg from 'particles-bg';
 import './about.scss';
 import Slide from 'react-reveal/Slide';
-import mypics from '../../resources/mypics.jpg'
+import mypics from '../../resources/samuel.jpg';
+import BlockRevealAnimation from 'react-block-reveal-animation';
 
 const About = () => {
     return (
-        <div className='root'>
+        <div id='about' className='about'>
             <div className='main'>
+            <div className='imagediv'>
+                    <Slide left duration={2000}>
+                        <img src={mypics} alt='my pics' className='mypics'/>
+                    </Slide>
+                </div>  
                 <div className='left'>
-                    <Slide top duration={3000}>
+                    <Slide top duration={2000}>
                         <h1>ABOUT ME</h1>
                     </Slide>
-                    <Slide right duration={3000}>
+                    <Slide right duration={2000}>
                         <div className='summary'>
-                            <p className='sum'>I am a detail-oriented and goal-driven software engineer, having over 2 years of experience in the IT domain seeking to pursue an opportunity with a growing organization that will utilize and enhance my knowledge in software development.</p>      
+                            <p className='sum'>I am a detail-oriented and goal-driven software engineer, having over 3 years of experience in the IT domain seeking to pursue an opportunity with a growing organization that will utilize and enhance my knowledge in software development.</p>      
                         </div>
                     </Slide>
                     <div className='education'>
-                        <Slide top duration={3000}>
+                        <BlockRevealAnimation duration={1.5} delay={2} color='#ff5757'>
                             <h2>Education</h2>
-                        </Slide>
-                        <Slide right duration={3000}>
+                        </BlockRevealAnimation>
+                        <Slide right duration={2000}>
                             <div>
                                 <h5>Abubakar Tafawa Balewa University</h5>
                                 <h5>B.Tech Estate Management and valuation</h5>
@@ -29,35 +34,59 @@ const About = () => {
                         </Slide> 
                     </div>
                 </div>
-                <div className='imagediv'>
-                    <Slide right duration={3000}>
-                        <img src={mypics} alt='my pics' className='mypics'/>
+                  
+            </div>
+            <div className="bottom">
+                <div className="bottom-left">
+                    {/* <BlockRevealAnimation duration={1.5} delay={2} color='#ff5757'> */}
+                        <h2>TOOLS</h2>
+                    {/* </BlockRevealAnimation> */}
+                    <Slide left duration={2500}>
+                        <h6>Languages</h6>
+                        <p>JavaScript, Typescript </p>
                     </Slide>
-                </div>    
+                    <Slide left duration={3000}>
+                        <h6>Frameworks & Libraries</h6>
+                        <p>React, React Native, Nodejs, Express, Angular</p>
+                    </Slide>
+                    <Slide left duration={3500}>
+                        <h6>Fundamentals</h6>
+                        <p>Html, Css, jQuery, Bootstrap</p>
+                    </Slide>
+                    <Slide left duration={4000}>
+                        <h6>Database</h6>
+                        <p>MySQL, MongoDB, FireStore</p>
+                    </Slide>
+                    
+                </div>
+                <div className='bottom-right'>
+                    <BlockRevealAnimation duration={1.5} delay={2} color='#ff5757'>
+                        <h2>SKILLS</h2>
+                    </BlockRevealAnimation>
+                    <Slide right duration={2500}>
+                        <label for='com'>Communication: </label>
+                        <progress id='com' value='90' max='100'></progress>
+                    </Slide>
+                    <Slide right duration={3000}>
+                        <label for='team'>Team Player: </label>
+                        <progress id='team' value='97' max='100'></progress>
+                    </Slide>
+                    <Slide right duration={3500}>
+                        <label for='inno'>Innovation: </label>
+                        <progress id='inno' value='91' max='100'></progress>
+                    </Slide>
+                    <Slide right duration={4000}>
+                        <label for='atd'>Attention to detail: </label>
+                        <progress id='atd' value='94' max='100'></progress>
+                    </Slide>
+                    <Slide bottom right duration={4500}>
+                        <label for='lead'>Leadership: </label>
+                        <progress id='lead' value='96' max='100'></progress>
+                    </Slide> 
+                </div>
             </div>
-            <div className='bottom'>
-                <Slide right duration={3000}>
-                    <h2>SKILLS</h2>
-                </Slide>
-                <Slide right duration={3000}>
-                    <label for='com'>Communication: </label>
-                    <progress id='com' value='90' max='100'></progress>
-                </Slide>
-                <Slide right duration={4000}>
-                    <label for='team'>Team Player: </label>
-                    <progress id='team' value='97' max='100'></progress>
-                </Slide>
-                <Slide right duration={5000}>
-                    <label for='inno'>Innovation: </label>
-                    <progress id='inno' value='91' max='100'></progress>
-                </Slide>
-                <Slide right duration={6000}>
-                    <label for='atd'>Attention to detail: </label>
-                    <progress id='atd' value='94' max='100'></progress>
-                </Slide>
-                
-            </div>
-            <ParticlesBg type='square' color="#00" num={10} bg='true'/>
+            
+            
         </div>
     )
 }
