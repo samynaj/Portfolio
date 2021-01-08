487512e131d43cc76cdf46cf780ad038dde87c9b
samynaj <samuelnnj@gmail.com>
2021-01-08 23:48:46 +0100

    worked on the home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,43 +1,54 @@
-import sandy from '../../resources/sandy.png'
-import Tada from 'react-reveal/Tada'
+import BlockRevealAnimation from 'react-block-reveal-animation';
 import Typewriter from 'typewriter-effect';
-import ParticlesBg from 'particles-bg'
+import Bounce from 'react-reveal/Bounce';
+import Slide from 'react-reveal/Slide';
+import resume from '../../resources/resume.pdf';
 
 import './home.scss'
 
 const Home = () => {
     return (
-        <div>
-            <div className='welcome-msg'>
-                <div className='welcome-msg__left'>
-                    <div className='msg-top'>
-                        <h1 className='name'>Hi, I am Samuel Nnaji.</h1>
-                    </div>
+        <div id='home' className='welcome-msg'>
+            <div className='welcome-msg__left'>
+                <div className='msg-top'>
+                    <BlockRevealAnimation duration={1.5} delay={2} color='#00c2cb'>
+                        <h1 className='name'>SAMUEL NNAJI</h1>
+                    </BlockRevealAnimation>
                     
-                    <div className='welcome'>
-                        <Typewriter
-                            options={{
-                            strings: ['A Full Stack Software Developer.', 'I build scalable digital products.'],
-                            autoStart: true,
-                            loop: true,
-                            delay: 120,
-                            deleteSpeed: 60
-                            }}
-                        />
-                    </div>
-                    <div className='msg-bottom'>
-                        <p>Plug me in for your projects or any available full time roles.</p>
-                    </div>
                 </div>
                 
-                <div className='image'>
-                    <Tada wait={3000} duration={3000}>
-                        <img className='img' src={sandy} alt='sandy'/>
-                    </Tada>
+                <div className='welcome'>
+                    <Typewriter
+                        options={{
+                        strings: ['I am a Full Stack Software Developer.', 'I build scalable digital products.'],
+                        autoStart: true,
+                        loop: true,
+                        delay: 120,
+                        deleteSpeed: 60
+                        }}
+                    />
+                </div>
+                <div className='msg-bottom'>
+                    <Bounce duration={3000}>
+                        <p>SOFTWARE DEVELOPER & TECHNICAL WRITER</p>
+                    </Bounce>
+                    
                 </div>
             </div>
-            <ParticlesBg type='square' color="#000" num={10} bg='true'/>
+            <div className="bottom-nav">
+                <a href={resume} target='_blank' rel='noreferrer'>
+                    <Slide left duration={3000}>
+                        <button className='download'>Download Resume</button>
+                    </Slide>
+                </a>
+                <a href='https://samynaj.hashnode.dev/' target='_blank' rel='noreferrer'>
+                    <Slide right duration={3000}>
+                        <button className='blog'>Blog</button>
+                    </Slide>
+                </a>
+            </div>
         </div>
+    
     )
 }
 
